897c2231a4fc4b9cfaf51fd1ec6fa1c8c624c581
yuui25 <fujizakk.southernrock25@gmail.com>
2025-07-18 13:40:59 -0400

    auth5

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -3,13 +3,13 @@ import functools
 from flask import(
     Blueprint,flash,g,redirect,render_template,request,session,url_for
 )
-from weakref.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
-bp = Blueprint('auth',__name__,url_prefix='/auth')
+bp = Blueprint('auth',__name__, url_prefix='/auth')
 
-@bp.route('/register',methods=('GET','POST'))
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
