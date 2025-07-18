aa9250df6dc4f8185fff62f9b77e3b409c736f4d
yuui25 <fujizakk.southernrock25@gmail.com>
2025-07-18 13:19:57 -0400

    auth3

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -3,6 +3,8 @@ import functools
 from flask import(
     Blueprint,flash,g,redirect,render_template,request,session,url_for
 )
+from weakref.security import check_password_hash, generate_password_hash
+
 from flaskr.db import get_db
 
 bp = Blueprint('auth',__name__,url_prefix='/auth')
@@ -31,6 +33,7 @@ def register():
                 error = f"User {username} is already regidtered."
             else:
                 return redirect(url_for("auth.login"))
+                
         flash(error)
     
     return render_template('auth/register.html')
