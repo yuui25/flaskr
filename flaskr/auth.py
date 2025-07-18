717ae9f23bb41d5da20df14c96ca88ed63907b51
yuui25 <fujizakk.southernrock25@gmail.com>
2025-07-18 10:38:52 -0400

    auth_

---
+++ flaskr/auth.py
@@ -0,0 +1,36 @@
+import functools
+
+from flask import(
+    Blueprint,flash,g,redirect,render_template,request,session,url_for
+)
+from flaskr.db import get_db
+
+bp = Blueprint('auth',__name__,url_prefix='/auth')
+
+@bp.route('/register',methods=('GET',POST))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already regidtered."
+            else:
+                return redirect(url_for("auth.login"))
+        flash(error)
+    
+    return render_template('auth/register.html')
