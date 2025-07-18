26128add403cd055e62995a4f41f07f10b3914f2
yuui25 <fujizakk.southernrock25@gmail.com>
2025-07-18 08:28:49 -0400

    __init__

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,18 +1,18 @@
 import os
 
-from flask import flask
+from flask import Flask
 
-def create_app(test_config=none):
+def create_app(test_config=None):
     #create and configure the app
-    app = flask(__name__, instance_rerative_config=true)
-    add.config.from_mapping(
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flask.sqlite'),
     )
 
-    if test_config is none:
+    if test_config is None:
         #load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=true)
+        app.config.from_pyfile('config.py', silent=True)
     else:
         #load the test config if passed in
         app.config.from_mapping(test_config)
