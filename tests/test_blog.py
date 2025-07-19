d159d6b1831c9865aede63ad1804a840d2c2231c
yuui25 <fujizakk.southernrock25@gmail.com>
2025-07-19 04:48:34 -0400

    test_auth

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -73,3 +73,13 @@ def test_create_update_validate(client, auth, path):
     auth.login()
     response = client.post(path, data={'title': '', 'body': ''})
     assert b'Title is required.' in response.data
+
+def test_delete(client, auth, app):
+    auth.login()
+    response = client.post('/1/delete')
+    assert response.headers['Location'] == "/"
+
+    with app.app_context():
+        db = get_db()
+        post = db.execute('SELECT * FROM post WHRER id = 1').fetchone()
+        assert post is None
